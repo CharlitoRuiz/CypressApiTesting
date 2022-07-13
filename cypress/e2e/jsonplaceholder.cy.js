d1c487e8d989d67a9e837d80d384c942e24ab550
Carlos Ruiz <cruizfreak@hotmail.com>
2022-07-12 22:16:39 -0600

    Inicio de proyecto de automatizacion de API Fake

---
+++ cypress/e2e/jsonplaceholder.cy.js
@@ -0,0 +1,58 @@
+///<reference types="Cypress" />
+describe('Probar los metodos del API', () => {
+  it('Metodo GET', () => {
+    cy.request('https://jsonplaceholder.typicode.com/posts/1').then(response => {
+      expect(response.status).to.eq(200);
+    });
+  });
+  it('Metodo POST', () => {
+    cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', 
+    {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+      Headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    .then(response => {
+      expect(response.status).to.eq(201);
+      expect(response.body).to.have.property('body', 'bar');
+    })
+  })
+  it('Metodo PUT', () => {
+    cy.request('PUT', 'https://jsonplaceholder.typicode.com/posts/1', 
+    {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    Headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }})
+    .then(response => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('title', 'foo');
+      console.log(response.body);
+    })
+  })
+  it('Metodo DELETE', () => {
+    cy.request('DELETE', 'https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+      expect(response.status).to.eq(200);
+      console.log(response.body);
+    })
+  })
+  it('Metodo PATCH', () => {
+    cy.request('PATCH', 'https://jsonplaceholder.typicode.com/posts/1', 
+    {
+      title: 'foo',
+    Headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      
+    }})
+    .then(response => {
+      expect(response.body.title).to.eq("foo");
+      console.log(response.body);
+    })
+  })
+})
